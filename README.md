1bb648e2221976749b21372d2d77f77dce5d6cac
YAMANE Nana <shigematsu.nana@gmail.com>
2013-08-31 23:56:37 +0900

    Add init-app

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 BEAR-Saturday-Extension-Setup - Local Pear Installer
 ========================================
 
-
-まだ作りかけです。
-
+Beta version.
 
 Summary
 --------------------
 
 下記のセットアップを行います。
-
     ```
     /path/to/your/project
-
+        |
+        +--- App
+        +--- htdocs
         |
         +--- libs
               |
@@ -23,14 +22,16 @@ Summary
                     +--- php
                           |
                           +--- BEAR ＜Here!
+        +--- tmp
+        +--- logs
+        +--- App.php
     ```
-
 
 1. cd /path/to/your/local/project
 2. 上記1. のディレクトリに、composer をインストールします
 3. 上記1. のディレクトリに、ファイル「composer.json」を新規作成して保存します
-4. composer install を実行します
-以上。
+4. composer install を実行することで、BEAR.Saturdayのスケルトン作成までを完了します。
+5. phpunit 等をグローバルのComposer環境にインストールします。
 
 
 Installation / Usage
@@ -38,7 +39,7 @@ Installation / Usage
 
 1. cd /path/to/your/local/project
 
-2. Download the [`composer.phar`](https://getcomposer.org/composer.phar) executable or use the installer.
+2. composerコマンドをまだインストールしていなければ、[`composer.phar`](https://getcomposer.org/composer.phar) をダウンロードして下さい。
 
     ```
     $ curl -sS https://getcomposer.org/installer | php
@@ -63,4 +64,6 @@ Installation / Usage
     }
     ```
 
-4. Run Composer: `php composer.phar install`
+4. Composerを実行: `php composer.phar install`
+
+5. Composerを実行: `composer global require 'h4cc/phpqatools'`
--- src/Installer.php
+++ src/Installer.php
@@ -10,10 +10,10 @@ class Installer
     public static function postInstall(Event $event = null)
     {
         $projectRoot = dirname(dirname(dirname(dirname(__DIR__))));
-        self::pearLocalSetup($projectRoot);
+        self::pearLocalSetup($event, $projectRoot);
     }
 
-    public static function pearLocalSetup($fullPath)
+    public static function pearLocalSetup(Event $event, $fullPath)
     {
         $libsPath = $fullPath . '/' . 'libs';
         if(file_exists($libsPath . '/.pearrc')) {
@@ -29,17 +29,24 @@ class Installer
         $commands[] = "pear -c {$libsPath}/.pearrc config-set preferred_state alpha";
         $commands[] = "pear -c {$libsPath}/.pearrc channel-update pear.php.net";
         $commands[] = "pear -c {$libsPath}/.pearrc install PEAR";
-        // 以降はグローバルのpearコマンドは使わない。古いpear環境だと下記エラーが出るため。
+        // 古いpear環境で下記のようなエラーになる場合、
+        // グローバル更新する権限がユーザに無いと upgrade ができないので、以降は libs/ 下のpearコマンドを使う。
         // （pear/HTTP_Request2 requires PEAR Installer (version >= 1.9.2), installed version is 1.9.1）
         $commands[] = "{$libsPath}/pear/pear -c {$libsPath}/.pearrc channel-discover pear.bear-project.net";
         // -a は不要
         $commands[] = "{$libsPath}/pear/pear -c {$libsPath}/.pearrc install bear/BEAR-beta";
 
-        echo '***** Installing...' . PHP_EOL;
+        $io = $event->getIO();
+        $io->write('<info>***** Installing...</info>');
         foreach($commands as $cmd) {
-            echo '** ' . $cmd . ' **' . PHP_EOL;
+            $io->write('<info>** ' . $cmd . ' **</info>');
             system($cmd, $retval);
         }
-        echo '***** Completed.' . PHP_EOL;
+        $io->write('<info>***** Installation Complete.</info>');
+
+        $io->write('<info>***** Init Application...</info>');
+        $cmd = "{$libsPath}/pear/bear -c {$libsPath}/.pearrc init-app {$fullPath}";
+        system($cmd, $retval);
+        $io->write('<info>***** Application Ok...</info>');
     }
 }
