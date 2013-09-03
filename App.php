d86fe0ac28479490d0826ea19be5be36266bceef
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-09-03 18:33:01 +0900

    fix app

--- App.php
+++ App.php
@@ -15,7 +15,8 @@
  */
 define('_BEAR_APP_HOME', realpath(dirname(__FILE__)));
 
-require __DIR__. '/libs/autload.php';
+// composer auto load
+require_once __DIR__ . '/libs/autoload.php';
 require_once 'BEAR.php';
 
 $bearMode = isset($_SERVER['bearmode']) ? $_SERVER['bearmode'] : 0;
